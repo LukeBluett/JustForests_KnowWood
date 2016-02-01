b6413474d64f74eff88f994c551554000a553ae1
Luke <lukebluett@mail.com>
2016-02-01 16:20:52 +0000

    Up to rough template done for web project

---
+++ JustForestsKnowWood/JS/tempTropJS.js
@@ -0,0 +1,16 @@
+﻿function myFunction(x,y) {
+  			if (y.checked) 
+  			{
+  				document.getElementById('header').style.backgroundColor = 'blue';
+      			document.getElementById('test2').style.backgroundColor = 'blue';
+      			document.getElementById('test').style.display = "none";
+      			document.getElementById('test2').style.display = "block";
+  			} 
+  			else 
+  			{
+  				document.getElementById('header').style.backgroundColor = 'red';
+      			document.getElementById('test').style.backgroundColor = 'red';
+      			document.getElementById('test').style.display = "block";
+      			document.getElementById('test2').style.display = "none";
+			}
+		}
